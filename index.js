82a1d414718e11f874aeb8781671846b39ef770b
Rebecca Lieberman <rebeccalieberman@gmail.com>
2016-03-29 23:15:46 -0400

    image creates bug, fixing that

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get('/twilio-callback', function(request, response) {
     // twiml.message('testing right back at you!');
     twiml.message(function(){ //this works but not when you try to send an image using this.media
       this.body('testing right back at you!');
-      this.media('http://animals.sandiegozoo.org/sites/default/files/juicebox_slides/owl_horned.jpg');
+      // this.media('http://animals.sandiegozoo.org/sites/default/files/juicebox_slides/owl_horned.jpg');
     });
   }
 
